e8fbe6bc3bd19d99ee17bc031203067510212be3
Reem AlWadaey <xreem.m@hotmail.com>
2019-03-29 14:33:28 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,64 @@
+# Memory Game Project
+
+## Table of Contents
+
+* [Description](#Description)
+    * [Features](#Features)
+* [Technologies](#Technologies)
+* [How to Play](#How-to-Play)
+* [Project Tree](#Project-Tree)
+* [Resources](#Resources)
+* [Contributing](#contributing)
+
+## Description
+
+This project is a Memory Game, in which the player should match the smillier cards
+within 120 seconds.
+
+### Features
+
+* Timer
+* Move Counter
+* Star Rating
+* Reset button
+
+## Technologies
+
+* ES6
+* HTML
+* CSS
+* Font Awesome Toolkit
+
+
+## How To Play
+
+- The player flips one card over to reveal its underlying symbol.
+- The player then turns over a second card, trying to find the corresponding card with the same symbol.
+- If the cards match, both cards stay flipped over.
+- If the cards do not match, both cards are flipped face down.
+
+
+##Project Tree
+
+* [README.md](./README.md)
+* [index.html](./index.html)
+* [css](./css)
+    * [app.css](./app.css)
+* [img](./img)
+    * [geometry2.png](./geometry2.png)
+* [js](./js)
+    * [app.js](./app.js)
+* [CONTRIBUTING.md](./CONTRIBUTING.md)
+
+## Resources
+
+* shuffle function from [here](http://stackoverflow.com/a/2450976)
+* gameTime function inspired from [here](https://stackoverflow.com/questions/31106189/create-a-simple-10-second-countdown)
+* Udacity Webinars 
+
+
+## Contributing
+
+This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+
+For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
---
+++ js/app.js
@@ -0,0 +1,207 @@
+/*
+ * Create a list that holds all of your cards
+ */
+const cardlist=["fa-diamond","fa-diamond",
+                "fa-paper-plane-o","fa-paper-plane-o",
+                "fa-anchor","fa-anchor",
+                "fa-bolt","fa-bolt",
+                "fa-cube","fa-cube",
+                "fa-leaf","fa-leaf",
+                "fa-bicycle","fa-bicycle",
+                "fa-bomb","fa-bomb"];
+
+/*
+ * Declaration of the variables
+ */
+let card1,card2; //card1 = first clicked card, card2 = second clicked card
+let lock=false; //lock on clicking more than two cards 
+let OpenCards=[]; //to insert the opened cards
+let matchedCards=[]; //to insert the matched cards
+let firstClick=false; //to check if the user click the cards for first time
+let movesNo=0; // to count the number of moves
+let sec = 120; //intialize the second to 120
+let starNo = 3; //intialize the number of stars to 3
+
+let timer=document.getElementById("time"); //to show the time
+let counter=document.getElementById("moves"); //to show the number of moves
+let restarticon = document.querySelector('.fa-repeat');  //restart icon
+const deck=document.querySelector('.deck'); //to insert the shuffle card in this variable
+
+
+/* TO SHUFFLE THE CARDS
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+function shuffleCards(){
+    const randomcards=shuffle(cardlist);
+    for(rcard of randomcards){
+        const listElement = document.createElement("li");
+        listElement.classList.add("card");
+        const icon=document.createElement("i");
+        icon.classList.add("fa");
+        icon.classList.add(rcard);
+        listElement.appendChild(icon);
+        deck.appendChild(listElement);
+    }
+
+}
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+/* GAME LOGIC
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+
+/*
+ * Declaration of the functions
+ */
+
+
+//function to perform action when the card is clicked
+function OpenedCard(){
+    //for eack click, increament the move counter
+    moveCounter();
+
+    //to prevent the clicking of more than 2 cards 
+    if(lock) return;
+    //if it's a first click
+    if(!firstClick){
+        card1=this;
+        card1.classList.add('open','show');
+        OpenCards.push(card1);
+        firstClick=true;
+    }
+    //if it's a second click
+    else{
+        card2=this;
+        card2.classList.add('open','show');
+        OpenCards.push(card2);
+        firstClick=false;
+        checkMatching();
+    }
+
+}
+
+
+//function to check the matched open cards
+function checkMatching(){
+
+    //if they match
+    if(card1.innerHTML === card2.innerHTML){
+        //to check if the user press the same card or not
+        if(OpenCards.indexOf(card1)===OpenCards.indexOf(card2)){
+            card1.classList.remove('open','show'); 
+            card2.classList.remove('open','show'); 
+        }
+        //if the user press distinct cards that have the same icon
+        else{
+            card1.classList.add('match');
+            card2.classList.add('match');
+            matchedCards.push(card1);
+            matchedCards.push(card2);
+            completeGame();
+        }
+    }
+    //if they don't match
+    else{
+        lock=true;
+        setTimeout(function(){
+            OpenCards.forEach(function(card){
+                card.classList.remove('open','show'); 
+            });
+        },1000);
+        lock=false;
+    }
+    
+}
+
+//function for showing message when the user complete the game
+function completeGame(){
+    if(matchedCards.length===16){
+        setTimeout(function(){
+            let remainingSec=120-sec;
+            let msg = confirm("Congratulations!\nYou finished with:\n"+ remainingSec +
+                    " seconds\n"+ movesNo +" moves\n"+ starNo +
+                    " stars\nDo you want to play again?");
+            timer.remove;
+            return msg ? window.location.reload() : window.close();
+        },1000);}
+}
+
+//function for counting the time 
+//https://stackoverflow.com/questions/31106189/create-a-simple-10-second-countdown
+function gameTime(){
+    let decrement = setInterval(function(){
+      timer.innerHTML= sec + " seconds";
+      --sec;
+      if(sec <= 0){
+        clearInterval(decrement);
+        timer.innerHTML = "Finished";
+        let msg = confirm("Time Out!\nDo you want to play again?");
+        return msg ? window.location.reload() : window.close();
+      }
+    }, 1000);
+}
+
+//function for counting the number of moves
+function moveCounter(){
+    ++movesNo;
+    counter.innerHTML= movesNo;
+    //check the stars for each increament of the move counter
+    showStars();
+}
+
+//function for show the stars based on the number 
+function showStars(){
+    star=document.querySelector('.fa-star');
+    if(movesNo === 25){
+        star.classList.remove('fa-star');
+        starNo=2;
+    }
+    else if(movesNo === 45){
+        star.classList.remove('fa-star');
+        starNo=1;
+    }
+}
+
+
+/*
+ * Start the execution
+ */
+
+//for each load of the page, shuffle the cards
+window.onload = shuffleCards();
+
+//to start counting the time
+gameTime();
+
+//reload the page when the restart button is clicked
+restarticon.addEventListener('click',function(){
+    window.location.reload();
+});
+
+//call the OpenedCard function when the user click any cards
+const AllCards = document.querySelectorAll('.card');//Declare AllCards variable after calling the shufflecard function
+AllCards.forEach(card => card.addEventListener('click',OpenedCard));
